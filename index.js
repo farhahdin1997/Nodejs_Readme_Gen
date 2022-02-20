1cf4a87d95599feb8542f8e8106a162ac22cdc2a
farhah din <farhahdin97@hotmail.com>
2022-02-20 14:17:00 +0000

    added a object storing the welcome questions

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 // TODO: Include packages needed for this application
-'use strict';
-const fs = require('fs');
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-inquirer.registerPrompt('recursive', require('./utils/my-inquirer-recursive.js'));
-const generateMarkdown = require('./utils/generateMarkdown.js');
+"use strict";
+const fs = require("fs");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+inquirer.registerPrompt(
+  "recursive",
+  require("./utils/my-inquirer-recursive.js")
+);
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// Welcome message before asking the questions
+
+const welcome = [
+  {
+    type: "confirm",
+    prefix: "\b",
+    name: "welcome",
+    message: chalk.cyanBright(
+      `Thanks for using my README.md generator! You will be presented with options for your README's sections and their respective contents. To begin hit 'y' or enter.`
+    ),
+  },
+];
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
